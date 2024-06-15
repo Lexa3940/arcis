5d644f10dfba2c39a7a4a78aae3208a21c9dd7d1
LordDaniil <131807764+LordDaniil@users.noreply.github.com>
2024-06-15 16:36:04 +0300

    Update Futi.vue

--- src/components/Futi.vue
+++ src/components/Futi.vue
@@ -1,43 +1,109 @@
 <template>
-  <footer
-    class="h-44 pt-10 w-4/5 mx-auto content-center auto-rows-auto pb-10 flex flex-row items-center justify-between"
-  >
-    <div class="flex flex-col items-start">
-      <ul class="">
-        <router-link to="/rabota"
-          ><li class="pt-4 text-xl max-w-xs border-solid">Работа в фирме "1С"</li></router-link
-        >
-        <router-link to="/vacansii"
-          ><li class="pb-4 text-xl max-w-xs border-solid">Вакансии в фирме "1С"</li></router-link
-        >
-        <p class="text-xs">ООО "АРКИС" © 2018 - 2024</p>
-      </ul>
-    </div>
-    <img height="100" width="250" src="D:\Дипломка\архив\vue-sait\public\trace.svg" alt="1C" />
-    <div class="flex flex-row space-x-3 pt-5">
-      <a href="https://api.whatsapp.com/send?phone=89054012969" target="_blank"
-        ><img
-          height="50"
-          width="50"
-          src="D:\Дипломка\архив\vue-sait\public\whatsapp-whats-app-svgrepo-com.svg"
-          alt="whatsapp"
-      /></a>
-
-      <a href="https://t.me/JackP1888" target="_blank" rel="noopener noreferrer"
-        ><img
-          height="50"
-          width="50"
-          src="D:\Дипломка\архив\vue-sait\public\telegram-svgrepo-com.svg"
-          alt="Telegram"
-      /></a>
-      <a href="https://vk.com/prostoghora" target="_blank" rel="noopener noreferrer">
-        <img
-          src="D:\Дипломка\архив\vue-sait\public\vk-1-logo-svgrepo-com.svg"
-          alt="ВКонтакте"
-          height="50"
-          width="50"
-        />
-      </a>
+  <footer class="footer">
+    <div class="footer__content">
+      <div class="footer__left">
+        <ul class="footer__list">
+          <router-link to="/rabota">
+            <li class="footer__list-item">Работа в фирме "1С"</li>
+          </router-link>
+          <router-link to="/vacansii">
+            <li class="footer__list-item">Вакансии в фирме "1С"</li>
+          </router-link>
+          <p class="footer__copyright">ООО "АРКИС" © 2018 - 2024</p>
+        </ul>
+      </div>
+      <div class="footer__logo">
+        <img src=".\архив\vue-sait\public\trace.svg" alt="1C" class="footer__logo-img" />
+      </div>
+      <div class="footer__social">
+        <a href="https://api.whatsapp.com/send?phone=89054012969" target="_blank" class="footer__social-link">
+          <img src=".\архив\vue-sait\public\whatsapp-whats-app-svgrepo-com.svg" alt="whatsapp" class="footer__social-icon" />
+        </a>
+        <a href="https://t.me/JackP1888" target="_blank" rel="noopener noreferrer" class="footer__social-link">
+          <img src=".\архив\vue-sait\public\telegram-svgrepo-com.svg" alt="Telegram" class="footer__social-icon" />
+        </a>
+        <a href="https://vk.com/prostoghora" target="_blank" rel="noopener noreferrer" class="footer__social-link">
+          <img src=".\архив\vue-sait\public\vk-1-logo-svgrepo-com.svg" alt="ВКонтакте" class="footer__social-icon" />
+        </a>
+      </div>
     </div>
   </footer>
 </template>
+
+<style scoped>
+.footer {
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+.footer__content {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.footer__left {
+  flex: 1;
+  margin-right: 20px;
+}
+
+.footer__list {
+  list-style: none;
+  padding: 0;
+}
+
+.footer__list-item {
+  font-size: 16px;
+  margin-bottom: 10px;
+  text-decoration: none;
+  color: #333;
+}
+
+.footer__copyright {
+  font-size: 14px;
+  color: #666;
+  margin-top: 10px;
+}
+
+.footer__logo {
+  flex: 1;
+  text-align: center;
+  margin-right: 20px;
+}
+
+.footer__logo-img {
+  max-width: 100%;
+  height: auto;
+}
+
+.footer__social {
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.footer__social-link {
+  margin-left: 10px;
+}
+
+.footer__social-icon {
+  width: 40px;
+  height: 40px;
+}
+
+@media (max-width: 768px) {
+  .footer__content {
+    flex-direction: column;
+  }
+
+  .footer__left,
+  .footer__logo,
+  .footer__social {
+    margin-bottom: 20px;
+  }
+}
+</style>
+
