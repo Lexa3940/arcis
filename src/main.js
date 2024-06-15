5c2a22b1330582054af76c976c957023cea52fe8
LordDaniil <131807764+LordDaniil@users.noreply.github.com>
2024-06-15 16:22:53 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import Home from './pages/Home.vue'
 import Favorites from './pages/Favorites.vue'
 import Profile from './pages/Profile.vue'
 import Conf from './pages/Conf.vue'
+import Rabota from './pages/Rabota.vue'
+import Vacansii from './pages/Vacansii.vue'
 
 const app = createApp(App)
 
@@ -15,7 +17,9 @@ const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/favorites', name: 'Favorites', component: Favorites },
   { path: '/profile', name: 'Profile', component: Profile },
-  { path: '/conf', name: 'Conf', component: Conf }
+  { path: '/conf', name: 'Conf', component: Conf },
+  { path: '/rabota', name: 'Rabota', component: Rabota },
+  { path: '/vacansii', name: 'Vacansii', component: Vacansii }
 ]
 
 const router = createRouter({
@@ -27,21 +31,3 @@ app.use(router)
 app.use(autoAnimatePlugin)
 
 app.mount('#app')
-
-/*import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/database'
-import 'firebase/messaging'
-import 'firebase/storage'
-
-const Config = {
-    apiKey: "AIzaSyCZZsNMxBiv-ZgvdGMsmQRLpmZu7K3AhXQ",
-    authDomain: "vue-arkis.firebaseapp.com",
-    projectId: "vue-arkis",
-    storageBucket: "vue-arkis.appspot.com",
-    messagingSenderId: "940662866254",
-    appId: "1:940662866254:web:3760d120e0ac345b6c1e1f",
-    measurementId: "G-R597K4PLGJ"
-  };
-
-  firebase.initializeApp(Config);*/
